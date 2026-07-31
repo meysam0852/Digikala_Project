290e3d80ed17f1c7d5346d31b6f3ced4a7f6158a
danial137 <danial79fakhrabadi@gmail.com>
2026-07-31 17:56:52 +0330

    Add CartItem and Order models with migrations for cart and orders apps

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
 # Create your models here.
+
+class CartItem(models.Model):
+    customer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="cart_items"
+    )
+    Product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name="cart_items"
+    )
+    
+    quantity = models.PositiveBigIntegerField(default=1)
+    
+    def _str_(self):
+        return f"{self.customer.username} - {self.product.name}"
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
 # Create your models here.
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="orders"
+    )
+    
+    total_amount = models.DecimalField(
+        max_digits=10,
+        decimal_places=2
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"Order #{self.id}"
