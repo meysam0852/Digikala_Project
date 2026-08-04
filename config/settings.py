dba60d56b8601be38b041b40b87ae616cdc7620e
mhks1999 <mhks1999@gmail.com>
2026-08-04 21:37:15 +0330

    fixed base and home

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_BANE'),
+        'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST', default='localhost'),
--- stores/urls.py
+++ stores/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.store_list, name="store-list"),
+    path("",views.store_list, name="store-list",),
     path("<int:id>/", views.store_detail, name="store-detail"),
     path("create/", views.create_store, name="create-store"),
     path("<int:id>/update/", views.update_store, name="update-store"),
